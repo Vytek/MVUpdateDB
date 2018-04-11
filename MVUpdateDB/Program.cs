9c6a39b063886bc621a82d45471b456236419456
Enrico Speranza <enrico.speranza@gmail.com>
2018-04-11 16:37:04 +0200

    First Commit

---
+++ MVUpdateDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MVUpdateDB
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
